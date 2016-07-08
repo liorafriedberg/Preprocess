3d5addf66db623fb53c57e3b24fa073991cbb9a5
Daniel Smullen <dsmullen@outbreaklabs.com>
2016-07-08 15:30:50 -0400

    Trying to fix repo weirdness.

---
+++ src/fileIO/CSVFile.java
@@ -0,0 +1,151 @@
+package fileIO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
+public class CSVFile {
+
+	private List<String> columns;
+	private File myFile;
+	private List<String[]> lines;
+
+	/**
+	 * Private constructor. Loads the contents of the CSV file into memory, and
+	 * creates a list of the columns.
+	 * 
+	 * @param filename
+	 *            The path and filename to the file.
+	 * @throws IOException
+	 *             This will get thrown if the filename can't be read.
+	 * @throws FileNotFoundException
+	 *             This will get thrown if the filename doesn't exist.
+	 */
+	private CSVFile(String filename) throws IOException, FileNotFoundException {
+		columns = new ArrayList<String>();
+		myFile = new File(filename);
+		lines = new ArrayList<String[]>();
+
+		CSVReader reader = new CSVReader(new FileReader(filename));
+		String[] nextLine;
+
+		while ((nextLine = reader.readNext()) != null) {
+			// nextLine[] is an array of values from the line
+			lines.add(nextLine);
+		}
+
+		String[] column_names = lines.get(0);
+
+		for (String col : column_names) {
+			columns.add(col);
+		}
+
+		// Remove the header row from the lines.
+		lines.remove(0);
+		reader.close();
+	}
+
+	/**
+	 * @param filename
+	 *            The path and filename to the file. Should work on all
+	 *            platforms. Relative paths supported.
+	 * @return Returns a CSVFile object.
+	 */
+	public static CSVFile read(String filename) {
+		try {
+			return new CSVFile(filename);
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: " + filename);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Something's broken!");
+			e.printStackTrace();
+		}
+
+		// If we failed to read the file, returns null.
+		return null;
+	}
+
+	public static boolean write(List<String> header, List<String> lines, File filename) {
+		//TODO: pull this method from AMTResultsFile
+		try {
+			FileWriter out;
+			out = new FileWriter(filename);
+			// Write the header.
+			out.write(String.join(",", header) + "\n");
+			out.flush();
+
+			// Write all lines.
+			for (String line : lines) {
+				out.write(line + "\n");
+				out.flush();
+			}
+
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @return Returns the number of value rows (number of rows in the CSV file,
+	 *         minus the header row).
+	 */
+	public int size() {
+		return lines.size();
+	}
+
+	public String getFilename() {
+		return myFile.toString();
+	}
+
+	public String[] getHeader() {
+		return columns.toArray(new String[0]);
+	}
+
+	public String[] getLine(int line) {
+		return lines.get(line);
+	}
+
+	public List<String[]> getAllLines() {
+		return lines;
+	}
+
+	/**
+	 * @param column_name
+	 *            The name of the column you want to retrieve. Column names are
+	 *            specified in the header row of the CSV file. Only the first
+	 *            instance of the column name specified will be returned. Column
+	 *            names need not be surrounded with quotes, a regular string
+	 *            will do.
+	 * @param row_number
+	 *            Row number that you want to retrieve. Zero indexed (0
+	 *            represents the first row of values, not the row header).
+	 * @return Returns the value at the corresponding row, or column. Null if
+	 *         the column or row wasn't found.
+	 */
+	public String getField(String column_name, int row_number) {
+		int whichColumn = columns.indexOf(column_name);
+
+		if (whichColumn == -1)
+			return null;
+
+		String value;
+
+		value = lines.get(row_number)[whichColumn];
+
+		return value;
+	}
+
+}
---
+++ src/fileIO/FileChooser.java
@@ -0,0 +1,180 @@
+package fileIO;
+
+import javax.swing.JFileChooser;
+
+import processing.ResultsProcessor;
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FileChooser extends JPanel implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	static private final String newline = "\n";
+	JButton inputButton1;
+	JButton inputButton2;
+	JButton saveButton;
+	JButton goButton;
+
+	File input1, input2, output = null;
+
+	JTextArea log;
+	JFileChooser fc;
+
+	public FileChooser() {
+		super(new BorderLayout());
+
+		// Create the log first, because the action listeners
+		// need to refer to it.
+		log = new JTextArea(5, 20);
+		log.setMargin(new Insets(5, 5, 5, 5));
+		log.setEditable(false);
+		JScrollPane logScrollPane = new JScrollPane(log);
+
+		// Create a file chooser
+		fc = new JFileChooser();
+
+		// fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		// fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+		// Create the open buttons.
+		inputButton1 = new JButton("Open SurveyGizmo Data File", createImageIcon("/images/Open16.gif"));
+		inputButton1.addActionListener(this);
+		inputButton2 = new JButton("Open MTurk Data File", createImageIcon("/images/Open16.gif"));
+		inputButton2.addActionListener(this);
+
+		// Create the save button.
+		saveButton = new JButton("Select Output File", createImageIcon("/images/Save16.gif"));
+		saveButton.addActionListener(this);
+
+		// Create the go button.
+		goButton = new JButton("Process", createImageIcon("/images/Go16.gif"));
+		goButton.addActionListener(this);
+
+		// For layout purposes, put the buttons in a separate panel
+		JPanel buttonPanel = new JPanel(); // use FlowLayout
+		buttonPanel.add(inputButton1);
+		buttonPanel.add(inputButton2);
+		buttonPanel.add(saveButton);
+		buttonPanel.add(goButton);
+
+		// Add the buttons and the log to this panel.
+		add(buttonPanel, BorderLayout.PAGE_START);
+		add(logScrollPane, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Opens a GUI that allows a user to choose a file to open.
+	 * 
+	 * @return Returns a File object if the user-specified file was successfully
+	 *         opened. Null if not.
+	 */
+	private File inputButtonAction() {
+		int returnVal = fc.showOpenDialog(FileChooser.this);
+		File file = null;
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			file = fc.getSelectedFile();
+			log.append("Opening: " + file.getName() + "." + newline);
+			if (!file.canRead()) {
+				log.append("Cannot open specified file for reading." + newline);
+				return null;
+			}
+		} else {
+			log.append("Open command cancelled by user." + newline);
+		}
+		log.setCaretPosition(log.getDocument().getLength());
+		return file;
+	}
+
+	private File saveButtonAction() {
+		int returnVal = fc.showSaveDialog(FileChooser.this);
+		File file = null;
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			file = fc.getSelectedFile();
+			log.append("Saving at: " + file.getName() + "." + newline);
+
+			try {
+				if (!file.createNewFile() && !file.canWrite()) {
+					log.append("Couldn't save file - can't write to selected file." + newline);
+					return null;
+				}
+			} catch (IOException e) {
+				log.append("Couldn't save file - I/O error." + newline);
+				e.printStackTrace();
+			}
+
+		} else {
+			log.append("Save command cancelled by user." + newline);
+		}
+		log.setCaretPosition(log.getDocument().getLength());
+		return file;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// Handle open button actions.
+		if (e.getSource() == inputButton1) {
+			File f = inputButtonAction();
+
+			// TODO: do the same thing with validation that we did with the
+			// AMTResultsFile object.
+			input1 = f;
+		} else if (e.getSource() == inputButton2) {
+			File f = inputButtonAction();
+			input2 = (AMTResultsFile.validate(f)) ? f : null;
+		}
+		// Handle save button action.
+		else if (e.getSource() == saveButton) {
+			output = saveButtonAction();
+		}
+		// Handle go button action.
+		else if (e.getSource() == goButton) {
+			doProcessing();
+		}
+	}
+
+	private void doProcessing() {
+		//Input1: SurveyGizmo Results
+		//Input2: MTurk Results
+		//Output: Output to write to.
+		if ((input1 != null) && (input2 != null)) {
+			if (output != null) {
+				ResultsProcessor r = new ResultsProcessor(input2, input1, output);
+
+				String[] results = r.process();
+				log.append("Processing complete." + newline);
+
+				if (results.length > 0) {
+					log.append("Errors were found in the data." + newline);
+					for (String line : results)
+						log.append(line + newline);
+				} else {
+					log.append("No errors were found. All checks passed." + newline);
+				}
+
+			} else {
+				log.append("Invalid output file specified, or output is not writeable." + newline);
+			}
+		} else {
+			log.append("Invalid input file(s) specified, or input could not be read." + newline);
+		}
+		log.setCaretPosition(log.getDocument().getLength());
+	}
+
+	protected static ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = FileChooser.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
+
+}
